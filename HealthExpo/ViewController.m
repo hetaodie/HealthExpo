789a669f89eb146f988204dfdfcd59883f5a060c
zhanyawei <yawei190@163.com>
2016-06-23 23:55:32 +0800

    1.加入tabbar，待调整。

--- HealthExpo/ViewController.m
+++ HealthExpo/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import "LoginViewController.h"
+#import "RegisterViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UITabBar *bottomView;
+@property (strong, nonatomic) IBOutlet UITabBarItem *firstTabBar;
+@property (strong, nonatomic) IBOutlet UITabBarItem *secondTabBar;
+@property (strong, nonatomic) IBOutlet UITabBarItem *thirdTabBar;
+@property (strong, nonatomic) IBOutlet UITabBarItem *fourthTabBar;
+@property (strong, nonatomic) IBOutlet UITabBarItem *fifthTabBar;
 
 @end
 
@@ -16,7 +24,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self createTabBar];
+    [self adjustViewControllers];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)createTabBar{
+    self.firstTabBar = [[UITabBarItem alloc] initWithTitle:@"首页" image:[UIImage imageNamed:@"shouye"] selectedImage:[UIImage imageNamed:@"shouye2"]];
+    self.secondTabBar = [[UITabBarItem alloc] initWithTitle:@"电话" image:[UIImage imageNamed:@"dianhua"] selectedImage:[UIImage imageNamed:@"dianhua2"]];
+    self.thirdTabBar = [[UITabBarItem alloc] initWithTitle:@"通讯录" image:[UIImage imageNamed:@"tongxunlu"] selectedImage:[UIImage imageNamed:@"tongxunlu2"]];
+    self.fourthTabBar = [[UITabBarItem alloc] initWithTitle:@"签到" image:[UIImage imageNamed:@"qiandao"] selectedImage:[UIImage imageNamed:@"qiandao2"]];
+    self.fifthTabBar = [[UITabBarItem alloc] initWithTitle:@"我的" image:[UIImage imageNamed:@"wode"] selectedImage:[UIImage imageNamed:@"wode2"]];
+}
+
+- (void)adjustViewControllers{
+    LoginViewController *loginVC = [[LoginViewController alloc] initWithNibName:@"LoginViewController" bundle:nil];
+    loginVC.tabBarItem = self.firstTabBar;
+    RegisterViewController *registerVC = [[RegisterViewController alloc] initWithNibName:@"RegisterViewController" bundle:nil];
+    registerVC.tabBarItem = self.secondTabBar;
+    
+    self.viewControllers = [NSArray arrayWithObjects:loginVC, loginVC, registerVC, registerVC, loginVC, nil];
 }
 
 - (void)didReceiveMemoryWarning {
