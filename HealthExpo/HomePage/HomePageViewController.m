0dc718412af07b29c436386a0ce6406c767f94a3
zhanyawei <yawei190@163.com>
2016-06-30 23:16:22 +0800

    1.首页调整 2.个人账号页面不可滑动

--- HealthExpo/HomePage/HomePageViewController.m
+++ HealthExpo/HomePage/HomePageViewController.m
@@ -24,14 +24,17 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - UIButtonClickedAction
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)onTopMsgClicked:(id)sender {
 }
-*/
+- (IBAction)onBaiKeClicked:(id)sender {
+}
+- (IBAction)onJiGouClicked:(id)sender {
+}
+- (IBAction)onZaZhiClicked:(id)sender {
+}
+- (IBAction)onGuaHaoClicked:(id)sender {
+}
 
 @end
