916c8fea82ac9cd25eb701ea557bede3013cb101
yawei zhan <yawei190@163.com>
2016-07-12 21:34:54 +0800

    1.添加疾病百科modelSource

---
+++ HealthExpo/HomePage/YiLiaoBaiKe/JiBingBKModelSource.h
@@ -0,0 +1,13 @@
+//
+//  JiBingBKModelSource.h
+//  HealthExpo
+//
+//  Created by hzzhanyawei on 16/7/12.
+//  Copyright © 2016年 AllWantsLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JiBingBKModelSource : NSObject
+
+@end
---
+++ HealthExpo/HomePage/YiLiaoBaiKe/JiBingBKModelSource.m
@@ -0,0 +1,13 @@
+//
+//  JiBingBKModelSource.m
+//  HealthExpo
+//
+//  Created by hzzhanyawei on 16/7/12.
+//  Copyright © 2016年 AllWantsLab. All rights reserved.
+//
+
+#import "JiBingBKModelSource.h"
+
+@implementation JiBingBKModelSource
+
+@end
