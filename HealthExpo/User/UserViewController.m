86ee1b259454b2e1ec350e17e78333f7e4c68216
yawei zhan <yawei190@163.com>
2016-08-14 16:38:54 +0800

    个人页面编辑。

--- HealthExpo/User/UserViewController.m
+++ HealthExpo/User/UserViewController.m
@@ -102,6 +102,7 @@
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
     if (indexPath.row == 0) {
         [self onOpenAlbum];
     }
